7c33386360d9660f1af6d795637822728680aad7
yunminseop <001208yun@gmail.com>
2025-09-21 23:12:07 +0900

    cpp_Course added

---
+++ cpp_course/1. cpp_start.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(){
+    std::cout << "Hello\n";
+    std::cout << "This is the first example.\n";
+    return 0;
+}
---
+++ cpp_course/2. using_namespace.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "using namespace can skip writing \"std::\".\n";
+    return 0;
+
+}
